16793826984cc7786779753d2ebf0548b55463a2
Arthur Carsten Morales Porto <arthur.porto@digitro.corp>
2026-01-14 17:12:48 -0300

    Aplicado a regra de soft-delete com o Paranoid do Sequelize em todas as entidades

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,31 +1,34 @@
-const dataSource = require('../models');
+const dataSource = require('../database/models');
 
 class Services {
     constructor(nomeDoModel) {
         this.model = nomeDoModel;
     }
-    
+
     async pegaTodosOsRegistros() {
         return dataSource[this.model].findAll();
     }
 
-    async pegaUmRegistroPorId(id){
-        return dataSource[this.model].findByPk();
+    async pegaUmRegistroPorId(id) {
+        return dataSource[this.model].findByPk(id);
     }
 
-    async criaRegistro(dadosDoRegistro){
+    async criaRegistro(dadosDoRegistro) {
         return dataSource[this.model].create(dadosDoRegistro);
     }
 
-    async excluiRegistro(id){
-        return dataSource[this.model].findByPk();
+    async excluiRegistro(id) {
+        return dataSource[this.model].destroy({
+            where: { id }
+        });
     }
 
-    async atualizaRegistro(dadosAtualizados, id){
+
+    async atualizaRegistro(dadosAtualizados, id) {
         const listaDeRegistrosAtualizado = dataSource[this.model].update(dadosAtualizados, {
-            where: {id: id}
+            where: { id: id }
         });
-        if(listaDeRegistrosAtualizado[0]===0){
+        if (listaDeRegistrosAtualizado[0] === 0) {
             return false;
         }
         return true;
